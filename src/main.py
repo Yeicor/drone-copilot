791669ad41764ef5e4906595306fc2bc088bcb81
Yeicor <4929005+Yeicor@users.noreply.github.com>
2022-08-16 17:29:25 +0200

    More failed testing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import os
 
 import numpy as np
+from kivy import Logger
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
@@ -21,15 +23,18 @@ class MyApp(App):
         root.add_widget(Video())
 
         # TensorsFlow demo
+        start_time = Clock.time()
         model = TensorFlowModel()
         model.load(os.path.join(os.getcwd(), 'model.tflite'))
+        Logger.info('TensorFlowModel load in ' + str(Clock.time() - start_time))
         np.random.seed(42)
         x = np.array(np.random.random_sample((1, 28, 28)), np.float32)
         y = model.pred(x)
+        Logger.info('TensorFlowModel prediction in ' + str(Clock.time() - start_time))
         # result should be
         # 0.01647118,  1.0278152 , -0.7065112 , -1.0278157 ,  0.12216613,
         # 0.37980393,  0.5839217 , -0.04283606, -0.04240461, -0.58534086
-        root.add_widget(Label(text=f'{y}'))
+        root.add_widget(Label(text=f'{y}', text_size=(None, 100)))
 
         return root
 
--- src/video.py
+++ src/video.py
@@ -2,6 +2,7 @@ import cv2
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.image import Image
+from kivy import Logger
 
 
 # noinspection PyUnresolvedReferences
@@ -10,23 +11,28 @@ class Video(Image):
     def __init__(self):
         super(Video, self).__init__()
         # TODO: Android fix for:
+        start_time = Clock.time()
         self.capture = cv2.VideoCapture(
             "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/sample-mp4-file.mp4")
+        Logger.info('Starting video capture in ' + str(Clock.time() - start_time))
         # cv2.namedWindow("CV2 Image")
         self.clock_event = Clock.schedule_interval(self.update, 1.0 / 33.0)
 
     def update(self, dt):
         # display image from cam in opencv window
+        start_time = Clock.time()
         ret, frame = self.capture.read()
         if frame is None:
             self.clock_event.cancel()
             return  # EOF
 
-        buf1 = cv2.flip(frame, 0)  # Flip vertically for OpenGL
-        buf = buf1.tostring()
+        # buf1 = cv2.flip(frame, 0)  # Flip vertically for OpenGL
+        buf = frame.tostring()
 
         if self.texture is None:
             # noinspection PyArgumentList
             self.texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        self.texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.texture.flip_vertical()
+        self.texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte', mipmap_generation=False)
         self.canvas.ask_update()
+        Logger.info('Video capture update took ' + str(Clock.time() - start_time))
