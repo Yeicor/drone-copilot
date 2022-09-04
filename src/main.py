8b2aeb31d2b9e73605040b13645506273db39f9a
Yeicor <yeicor@users.noreply.github.com>
2022-09-04 10:29:49 +0000

    Update __version__ to 0.2.10 (automatic)

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from kivy.utils import platform
 
 from ui.app import DroneCopilotApp
 
-__version__ = '0.2.9'
+__version__ = '0.2.10'
 
 if __name__ == '__main__':
     if platform != 'android' and platform != 'ios':
